871ff195781fe9c06b43e3fd1ca54d74a2350e65
LuWeiQianYi <runningriven@163.com>
2023-08-24 18:05:08 +0800

    test: 增加测试接口(unregister logout两个接口返回400，TODO:fix)

--- cmd/account/api/test/api_test.go
+++ cmd/account/api/test/api_test.go
@@ -16,17 +16,56 @@ const (
 
 	accountNameKey = "accountName"
 	passwordKey    = "password"
-
-	token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoie1wiYWNjb3VudE5hbWVcIjpcImxlZWJhaVwifSIsImV4cCI6MTY5NDA4MDA0Nn0.uzPqBT-de3nb1OhKDWqO5XUzGYdvwgKl4qCj4SCLKJQ"
 )
+
+func TestRegister(t *testing.T) {
+	url := fmt.Sprintf("http://%v:%v%v", auth_api_host, port, register)
+
+	resp, err := resty.New().R().
+		SetBody(map[string]interface{}{accountNameKey: "leebai", passwordKey: "123456"}).
+		Post(url)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	fmt.Printf("resp: %v\n", resp)
+}
+
+func TestUnRegister(t *testing.T) {
+	url := fmt.Sprintf("http://%v:%v%v", auth_api_host, port, unregister)
+
+	resp, err := resty.New().R().
+		SetQueryParams(map[string]string{accountNameKey: "leebai"}).
+		Get(url)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	fmt.Printf("resp: %v\n", resp)
+}
 
 func TestLogin(t *testing.T) {
 	url := fmt.Sprintf("http://%v:%v%v", auth_api_host, port, login)
 
 	resp, err := resty.New().R().
-		SetHeader("Access-Token", token).
 		SetBody(map[string]interface{}{accountNameKey: "leebai", passwordKey: "123456"}).
 		Post(url)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	fmt.Printf("resp: %v\n", resp)
+}
+
+func TestLogout(t *testing.T) {
+	url := fmt.Sprintf("http://%v:%v%v", auth_api_host, port, logout)
+
+	resp, err := resty.New().R().
+		SetQueryParams(map[string]string{accountNameKey: "leebai"}).
+		Get(url)
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
 		return
